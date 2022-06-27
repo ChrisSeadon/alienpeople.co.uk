1bd865f8cfec992530fbaf61691838ce70cd7d58
Chris <seadonchristopher@gmail.com>
2022-06-27 23:44:53 +0100

    add everything
    nice

---
+++ src/config.js
@@ -0,0 +1,5 @@
+// using module.exports here so gatsby-config can read it
+module.exports = {
+  // TODO: Replace next line with your own public API key
+  builderAPIKey: '6be8d060b0384e4abc1615ba33098595'
+};
